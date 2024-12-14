3380acd8273807362dd157afff1f2bbe7f8ed365
Hamzaishfaq86 <hamzaishafaq73@gmail.com>
2024-12-14 00:38:36 -0800

    polaris

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
+
+import '@shopify/polaris/build/esm/styles.css';
+
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot, } from 'react-dom/client'
+import { AppProvider, } from '@shopify/polaris';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <AppProvider>
   <StrictMode>
     <App />
   </StrictMode>,
+  </AppProvider>
 )
